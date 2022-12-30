e6523cb57ef653d0071d13b86751022839f4b19a
stcalc <119763764+stcalc@users.noreply.github.com>
2022-12-30 01:00:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert('الموقع تحت الصيانة ارجع ورا ليكونطرول تلقاه يمشي');
+window.alert('الموقع تحت الصيانة ارجع ورا ليكونطرول تلقاه يمشي');
 function calcu() {
 
   var ph1ntd = Number(document.getElementById('ph1ntd').value) ;
