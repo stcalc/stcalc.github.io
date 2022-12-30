9c101ac47e9650dae2d4beacd7f330acc6b5eb49
stcalc <119763764+stcalc@users.noreply.github.com>
2022-12-30 17:45:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function calcu() {
 
   var ph1ntd = Number(document.getElementById('ph1ntd').value) ;
 
-  
+ 
 
   var ph1ctrl = Number(document.getElementById('ph1ctrl').value);
 
   var ph1ntp = Number(document.getElementById('ph1ntp').value);
 
+ 
 
-
-  var ph1tt = document.getElementById('ph1tt').value =  Number((ph1ntd)*0.2+(ph1ctrl*0.6)+(ph1ntp*0.2));
+  var ph1tt = document.getElementById('ph1tt').value =  Number((ph1ntd)*0.2+(ph1ctrl*0.6)+(ph1ntp)*0.2);
 
 
 
@@ -18,24 +18,24 @@ function calcu() {
 
   var ch1ntd = Number(document.getElementById('ch1ntd').value) ;
 
-  var ch1intr = Number(document.getElementById('ch1intr').value) ;
 
   var ch1ctrl = Number(document.getElementById('ch1ctrl').value);
 
   var ch1ntp = Number(document.getElementById('ch1ntp').value);
 
-  var ch1nctp = Number(document.getElementById('ch1nctp').value);
+  
 
-  var ch1tt = document.getElementById('ch1tt').value = Number((ch1ntd+*0.2)+(ch1ctrl*0.6)+(ch1ntp*0.2));
+  var ch1tt = document.getElementById('ch1tt').value = Number((ch1ntd)*0.2+(ch1ctrl*0.6)+(ch1ntp*0.2));
 
 
 
   var info1ntd = Number(document.getElementById('info1ntd').value) ;
 
 
+
   var info1ctrl = Number(document.getElementById('info1ctrl').value);
 
-  var info1tt = document.getElementById('info1tt').value = Number(((info1ntd*4)+(info1ctrl*6));
+  var info1tt = document.getElementById('info1tt').value = Number((info1ntd+info1ctrl)/2);
 
 
 
@@ -43,9 +43,11 @@ function calcu() {
 
     var alg1ntd = Number(document.getElementById('alg1ntd').value) ;
 
+   
+
     var alg1ctrl = Number(document.getElementById('alg1ctrl').value);
 
-    var alg1tt = document.getElementById('alg1tt').value = Number(((alg1ntd*4)+(alg1ctrl*6));
+    var alg1tt = document.getElementById('alg1tt').value = Number((((alg1ntd)*4)+(alg1ctrl*6))/10);
 
 
 
@@ -53,11 +55,11 @@ function calcu() {
 
     var ana1ntd = Number(document.getElementById('ana1ntd').value) ;
 
- 
+   
 
     var ana1ctrl = Number(document.getElementById('ana1ctrl').value);
 
-    var ana1tt = document.getElementById('ana1tt').value = Number(((ana1ntd*4)+(ana1ctrl*6));
+    var ana1tt = document.getElementById('ana1tt').value = Number((((ana1ntd)*4)+(ana1ctrl*6))/10);
 
 
 
@@ -69,7 +71,7 @@ function calcu() {
 
     var pro1ctrl = Number(document.getElementById('pro1ctrl').value);
 
-    var pro1tt = document.getElementById('pro1tt').value = Number(((pro1ntd*4)+(pro1ctrl*6));
+    var pro1tt = document.getElementById('pro1tt').value = Number((((pro1ntd)*4)+(pro1ctrl*6))/10);
 
 
 
