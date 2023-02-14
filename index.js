352efc12abf724864eb4265fb3c5854c9c3652ee
stcalc <119763764+stcalc@users.noreply.github.com>
2023-02-14 03:34:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("ارجو دعواتكم لي لنيل شهادة البكالوريا بمعدل جيد");
+alert(" (yah04dev) الموقع من تطويرالطالب يحي بن عمروش /nارجو دعواتكم لي لنيل شهادة البكالوريا بمعدل جيد");
 function calcu() {
 
   var ph1ntd = Number(document.getElementById('ph1ntd').value) ;
